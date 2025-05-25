3d4f4b58a4afb5552bead972d1da4fbd7a41b05c
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-25 20:16:45 +0300

    added extensions

--- Program.cs
+++ Program.cs
@@ -1,24 +1,15 @@
-using Microsoft.EntityFrameworkCore;
 using ModulesForSubtree.BannersModule.Extensions;
-using ModulesForSubtree.BannersModule.Services;
-using ModulesForSubtree.CampaignsModule.Contexts;
 using ModulesForSubtree.CampaignsModule.Extensions;
-using ModulesForSubtree.CampaignsModule.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddTransient<IBannersService, BannersService>();
-builder.Services.AddTransient<ICampaignsService, CampaignsService>();
-
 var connectionString = "Server=localhost;Initial Catalog=ModulesForSubtreeStorage;Integrated Security=True;TrustServerCertificate=True;";
 
 builder.Services.AddBannersModule(connectionString);
 builder.Services.AddCampaignsModule(connectionString);
-
-builder.Services.AddDbContext<CampaignsModuleDbContext>(opt => opt.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
