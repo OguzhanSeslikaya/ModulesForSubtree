b87b19632f768d84a34ba8d92c7dc2181272c8d4
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-26 01:13:13 +0300

    .

--- BannersModule/Contexts/BannersModuleDbContext.cs
+++ BannersModule/Contexts/BannersModuleDbContext.cs
@@ -6,6 +6,7 @@ namespace ModulesForSubtree.BannersModule.Contexts
     public class BannersModuleDbContext : DbContext
     {
         public DbSet<Banner> Banners { get; set; }
+        public DbSet<Entity1> Entity1s { get; set; }
 
         public BannersModuleDbContext(DbContextOptions<BannersModuleDbContext> options) : base(options)
         {
