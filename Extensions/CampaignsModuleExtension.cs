d46cf0329960afc1dd7b52f199bacc408ab1fc6b
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-25 20:13:38 +0300

    Apply .gitignore rules

---
+++ Extensions/CampaignsModuleExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModulesForSubtree.CampaignsModule.Contexts;
+using ModulesForSubtree.CampaignsModule.Services;
+
+namespace ModulesForSubtree.CampaignsModule.Extensions
+{
+    public static class CampaignsModuleExtension
+    {
+        public static void AddCampaignsModule(this IServiceCollection services, string sqlServerConnectionString)
+        {
+            services.AddDbContext<CampaignsModuleDbContext>(opt => opt.UseSqlServer(sqlServerConnectionString));
+
+            services.AddTransient<ICampaignsService, CampaignsService>();
+        }
+    }
+}
