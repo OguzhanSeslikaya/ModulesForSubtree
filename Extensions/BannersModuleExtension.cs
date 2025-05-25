e3622f99dce99be49b8e195b621cb7c737459092
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-25 20:13:38 +0300

    Apply .gitignore rules

---
+++ Extensions/BannersModuleExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModulesForSubtree.BannersModule.Contexts;
+using ModulesForSubtree.BannersModule.Services;
+
+namespace ModulesForSubtree.BannersModule.Extensions
+{
+    public static class BannersModuleExtension
+    {
+        public static void AddBannersModule(this IServiceCollection services, string sqlServerConnectionString)
+        {
+            services.AddDbContext<BannersModuleDbContext>(opt => opt.UseSqlServer(sqlServerConnectionString));
+
+            services.AddTransient<IBannersService, BannersService>();
+        }
+    }
+}
