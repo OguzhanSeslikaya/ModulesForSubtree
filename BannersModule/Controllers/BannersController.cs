37277cd8d6f5ebdf94edfa7c2ac176873f0208f4
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-26 09:13:11 +0300

    .

--- BannersModule/Controllers/BannersController.cs
+++ BannersModule/Controllers/BannersController.cs
@@ -28,5 +28,11 @@ namespace ModulesForSubtree.BannersModule.Controllers
         {
             return Ok();
         }
+
+        [HttpGet("Conflict2")]
+        public async Task<IActionResult> Conflict2()
+        {
+            return Ok();
+        }
     }
 }
